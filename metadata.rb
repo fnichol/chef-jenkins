2f9fc0391b57c9154bb21c039d55bfb4f6d30a72
Fletcher Nichol <fnichol@nichol.ca>
2011-07-03 15:49:37 -0600

    Begin reformatting of metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,21 @@ license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
-%w(runit java iptables).each { |cb| depends cb }
+
+recipe "jenkins",                 "Installs a Jenkins CI server"
+recipe "jenkins::node_ssh",       ""
+recipe "jenkins::node_jnlp",      ""
+recipe "jenkins::node_windows",   ""
+recipe "jenkins::iptables",       ""
+recipe "jenkins::proxy_apache2",  ""
+recipe "jenkins::proxy_nginx",    ""
+
+%w{ debian ubuntu centos redhat }.each do |os|
+  supports os
+end
+
+%w{ runit java }.each do |cb|
+  depends cb
+end
+
+recommends "iptables"
