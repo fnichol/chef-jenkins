ef65b0fcfd1ba7c44b4d1756cb1b334c162f7ff6
Fletcher Nichol <fnichol@nichol.ca>
2011-07-03 21:26:47 -0600

    Refactorings for recipe[jenkins::default].

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,45 +34,48 @@ pkey = "#{home_path}/.ssh/id_rsa"
 tmp = "/tmp"
 
 user server_user do
-  home home_path
+  home  home_path
 end
 
 directory home_path do
-  recursive true
-  owner server_user
-  group server_group
+  recursive   true
+  owner       server_user
+  group       server_group
 end
 
 directory "#{home_path}/.ssh" do
-  mode 0700
-  owner server_user
-  group server_group
+  mode    0700
+  owner   server_user
+  group   server_group
 end
 
 execute "ssh-keygen -f #{pkey} -N ''" do
-  user  server_user
-  group server_group
-  not_if { File.exists?(pkey) }
+  user    server_user
+  group   server_group
+
+  not_if  { ::File.exists?(pkey) }
 end
 
 ruby_block "store jenkins ssh pubkey" do
   block do
-    node.set['jenkins']['server']['pubkey'] = File.open("#{pkey}.pub") { |f| f.gets }
+    node.set['jenkins']['server']['pubkey'] =
+      ::File.open("#{pkey}.pub") { |f| f.gets }
   end
 end
 
 directory "#{home_path}/plugins" do
-  owner server_user
-  group server_group
-  only_if { plugins.size > 0 }
+  owner     server_user
+  group     server_group
+
+  not_if    { plugins.empty? }
 end
 
 plugins.each do |name|
   remote_file "#{home_path}/plugins/#{name}.hpi" do
-    source "#{mirror_url}/latest/#{name}.hpi"
-    backup false
-    owner server_user
-    group server_group
+    source  "#{mirror_url}/latest/#{name}.hpi"
+    backup  false
+    owner   server_user
+    group   server_group
   end
 end
 
@@ -93,12 +96,12 @@ when "ubuntu", "debian"
     package "psmisc"
 
     remote_file "#{tmp}/jenkins-ci.org.key" do
-      source "#{mirror_url}/debian/jenkins-ci.org.key"
+      source  "#{mirror_url}/debian/jenkins-ci.org.key"
     end
 
     execute "add-jenkins-key" do
-      command "apt-key add #{tmp}/jenkins-ci.org.key"
-      action :nothing
+      command   "apt-key add #{tmp}/jenkins-ci.org.key"
+      action    :nothing
     end
 
   when "ubuntu"
@@ -122,21 +125,21 @@ when "ubuntu", "debian"
     end
 
     cookbook_file "/etc/apt/sources.list.d/jenkins.list" do
-      owner "root"
-      group "root"
-      mode  "0644"
+      owner   "root"
+      group   "root"
+      mode    "0644"
     end
 
     execute "add-jenkins-key" do
-      command "curl #{key_url} | apt-key add -"
-      action :nothing
-      notifies :run, "execute[apt-get update]", :immediately
+      command   "curl #{key_url} | apt-key add -"
+
+      action    :nothing
+      notifies  :run, "execute[apt-get update]", :immediately
     end
   end
 
   pid_file = "/var/run/jenkins/jenkins.pid"
   install_starts_service = true
-
 
 when "centos", "redhat"
   #see http://jenkins-ci.org/redhat/
@@ -147,14 +150,14 @@ when "centos", "redhat"
   install_starts_service = false
 
   execute "add-jenkins-key" do
-    command "rpm --import #{mirror_url}/redhat/jenkins-ci.org.key"
-    action :nothing
+    command   "rpm --import #{mirror_url}/redhat/jenkins-ci.org.key"
+    action    :nothing
   end
 
 end
 
-#"jenkins stop" may (likely) exit before the process is actually dead
-#so we sleep until nothing is listening on jenkins.server.port (according to netstat)
+# "jenkins stop" may (likely) exit before the process is actually dead # so we
+# sleep until nothing is listening on jenkins.server.port (according to netstat)
 ruby_block "netstat" do
   block do
     10.times do
@@ -167,6 +170,7 @@ ruby_block "netstat" do
       sleep 1
     end
   end
+
   action :nothing
 end
 
@@ -187,79 +191,84 @@ ruby_block "block_until_operational" do
       sleep 1
     end
   end
+
   action :nothing
 end
 
+# `jenkins status` will exit(0) even when the process is not running
 service "jenkins" do
-  supports [ :stop, :start, :restart, :status ]
-  #"jenkins status" will exit(0) even when the process is not running
-  status_command "test -f #{pid_file} && kill -0 `cat #{pid_file}`"
-  action :nothing
+  supports        [ :stop, :start, :restart, :status ]
+  status_command  "test -f #{pid_file} && kill -0 `cat #{pid_file}`"
+  action          :nothing
 end
 
 if node.platform == "ubuntu"
   execute "setup-jenkins" do
-    command "echo w00t"
-    notifies :stop, "service[jenkins]", :immediately
-    notifies :create, "ruby_block[netstat]", :immediately #wait a moment for the port to be released
-    notifies :run, "execute[add-jenkins-key]", :immediately
-    notifies :install, "package[jenkins]", :immediately
+    command     "echo w00t"
+    notifies    :stop,    "service[jenkins]", :immediately
+    notifies    :create,  "ruby_block[netstat]", :immediately
+    notifies    :run,     "execute[add-jenkins-key]", :immediately
+    notifies    :install, "package[jenkins]", :immediately
     unless install_starts_service
-      notifies :start, "service[jenkins]", :immediately
+      notifies  :start,   "service[jenkins]", :immediately
     end
-    notifies :create, "ruby_block[block_until_operational]", :immediately
-    creates "/usr/share/jenkins/jenkins.war"
+    notifies    :create,  "ruby_block[block_until_operational]", :immediately
+
+    creates     "/usr/share/jenkins/jenkins.war"
   end
 else
-  local = File.join(tmp, File.basename(remote))
+  local = ::File.join(tmp, ::File.basename(remote))
 
   remote_file local do
-    source remote
-    backup false
-    notifies :stop, "service[jenkins]", :immediately
-    notifies :create, "ruby_block[netstat]", :immediately #wait a moment for the port to be released
-    notifies :run, "execute[add-jenkins-key]", :immediately
-    notifies :install, "package[jenkins]", :immediately
+    source      remote
+    backup      false
+    notifies    :stop, "service[jenkins]", :immediately
+    notifies    :create, "ruby_block[netstat]", :immediately
+    notifies    :run, "execute[add-jenkins-key]", :immediately
+    notifies    :install, "package[jenkins]", :immediately
     unless install_starts_service
-      notifies :start, "service[jenkins]", :immediately
+      notifies  :start, "service[jenkins]", :immediately
     end
+
     if node['jenkins']['server']['use_head'] #XXX remove when CHEF-1848 is merged
       action :nothing
     end
   end
 
   http_request "HEAD #{remote}" do
-    only_if { node['jenkins']['server']['use_head'] } #XXX remove when CHEF-1848 is merged
-    message ""
-    url remote
-    action :head
-    if File.exists?(local)
-      headers "If-Modified-Since" => File.mtime(local).httpdate
+    message   ""
+    url       remote
+    action    :head
+    if ::File.exists?(local)
+      headers "If-Modified-Since" => ::File.mtime(local).httpdate
     end
-    notifies :create, "remote_file[#{local}]", :immediately
+
+    notifies  :create, "remote_file[#{local}]", :immediately
+    only_if   { node['jenkins']['server']['use_head'] } #XXX remove when CHEF-1848 is merged
   end
 end
 
-#this is defined after http_request/remote_file because the package
-#providers will throw an exception if `source' doesn't exist
+# this is defined after http_request/remote_file because the package
+# providers will throw an exception if `source' doesn't exist
 package "jenkins" do
-  provider package_provider
-  source local if node.platform != "ubuntu"
-  action :nothing
+  provider  package_provider
+  source    local if node.platform != "ubuntu"
+  action    :nothing
 end
 
 # restart if this run only added new plugins
 log "plugins updated, restarting jenkins" do
-  #ugh :restart does not work, need to sleep after stop.
-  notifies :stop, "service[jenkins]", :immediately
-  notifies :create, "ruby_block[netstat]", :immediately
-  notifies :start, "service[jenkins]", :immediately
-  notifies :create, "ruby_block[block_until_operational]", :immediately
+  # ugh :restart does not work, need to sleep after stop.
+  notifies    :stop, "service[jenkins]", :immediately
+  notifies    :create, "ruby_block[netstat]", :immediately
+  notifies    :start, "service[jenkins]", :immediately
+  notifies    :create, "ruby_block[block_until_operational]", :immediately
+
   only_if do
-    if File.exists?(pid_file)
-      htime = File.mtime(pid_file)
+    if ::File.exists?(pid_file)
+      htime = ::File.mtime(pid_file)
       Dir["#{home_path}/plugins/*.hpi"].select { |file|
-        File.mtime(file) > htime
+        ::File.mtime(file) > htime
       }.size > 0
     end
   end
