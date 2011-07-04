8dbf043b420c1129ed9ae3d1d48a2d3f02a6ee0e
Fletcher Nichol <fnichol@nichol.ca>
2011-07-03 21:09:36 -0600

    Fix metadata link to README.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.5"
 
 recipe "jenkins",                 "Installs a Jenkins CI server"
