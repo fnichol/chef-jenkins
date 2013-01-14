3031795d24f11421321fc4f97dbb099033d9948f
Ashton Kemerling <akemerling@narrativescience.com>
2013-01-13 18:57:22 -0600

    Wrong comment char (!), and bad imports.

--- files/default/node_info.groovy
+++ files/default/node_info.groovy
@@ -1,28 +1,32 @@
-#
-# Cookbook Name:: jenkins
-# Based on hudson
-# File:: node_info
-#
-# Author:: Doug MacEachern <dougm@vmware.com>
-# Author:: Fletcher Nichol <fnichol@nichol.ca>
+//
+// Cookbook Name:: jenkins
+// Based on hudson
+// File:: node_info
+//
+// Author:: Doug MacEachern <dougm@vmware.com>
+// Author:: Fletcher Nichol <fnichol@nichol.ca>
 
-# Copyright 2010, VMware, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+// Copyright 2010, VMware, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
-import jenkins.slaves.*
-import jenkins.model.*
+import hudson.slaves.JNLPLauncher;
+import hudson.slaves.CommandLauncher;
+import hudson.model.Slave;
+import hudson.slaves.RetentionStrategy;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import jenkins.model.Jenkins;
 
 def toJSON(node) {
   if (node instanceof Map) {
