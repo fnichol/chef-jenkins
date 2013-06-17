47ad1349098e2c17f8f19e171ce850f512cad5fa
JJ Asghar <jjasghar@gmail.com>
2013-06-17 13:51:11 -0500

    Update default.rb
    It seems when i attempted to upload the cookbook, and ran it it asked for the jenkins name.  i believe it defaults to chef-jenkins so heres the fix :)
    
    ```
    [2013-06-17T18:49:41+00:00] WARN: Cloning resource attributes for execute[apt-get update] from prior resource (CHEF-3694)
    [2013-06-17T18:49:41+00:00] WARN: Previous execute[apt-get update]: /var/chef/cache/cookbooks/ops/recipes/packages.rb:12:in `from_file'
    [2013-06-17T18:49:41+00:00] WARN: Current  execute[apt-get update]: /var/chef/cache/cookbooks/apt/recipes/default.rb:29:in `from_file'
    
    ================================================================================
    Recipe Compile Error in /var/chef/cache/cookbooks/chef-jenkins/recipes/default.rb
    ================================================================================
    
    
    Chef::Exceptions::CookbookNotFound
    ----------------------------------
    Cookbook jenkins not found. If you're loading jenkins from another cookbook, make sure you configure the dependency in your metadata
    
    
    Cookbook Trace:
    ---------------
      /var/chef/cache/cookbooks/chef-jenkins/recipes/default.rb:256:in `from_file'
    
    
    Relevant File Content:
    ----------------------
    /var/chef/cache/cookbooks/chef-jenkins/recipes/default.rb:
    
    249:  end
    250:
    251:  # Front Jenkins with an HTTP server
    252:  case node[:jenkins][:http_proxy][:variant]
    253:  when "nginx"
    254:    include_recipe "jenkins::proxy_nginx"
    255:  when "apache2"
    256>>   include_recipe "jenkins::proxy_apache2"
    257:  end
    258:
    
    
    [2013-06-17T18:49:41+00:00] ERROR: Running exception handlers
    [2013-06-17T18:49:41+00:00] FATAL: Saving node information to /var/chef/cache/failed-run-data.json
    [2013-06-17T18:49:41+00:00] ERROR: Exception handlers complete
    Chef Client failed. 0 resources updated
    [2013-06-17T18:49:41+00:00] FATAL: Stacktrace dumped to /var/chef/cache/chef-stacktrace.out
    [2013-06-17T18:49:41+00:00] FATAL: Chef::Exceptions::CookbookNotFound: Cookbook jenkins not found. If you're loading jenkins from another cookbook, make sure you configure the dependency in your metadata
    ```

--- recipes/default.rb
+++ recipes/default.rb
@@ -251,7 +251,7 @@ end
 # Front Jenkins with an HTTP server
 case node[:jenkins][:http_proxy][:variant]
 when "nginx"
-  include_recipe "jenkins::proxy_nginx"
+  include_recipe "chef-jenkins::proxy_nginx"
 when "apache2"
-  include_recipe "jenkins::proxy_apache2"
+  include_recipe "chef-jenkins::proxy_apache2"
 end
